7ebc5182d8b2783ced70a5a9967b745d5cfa26a5
irenecoder <wairene60@gmail.com>
2022-12-28 21:48:29 +0300

    product model

--- kili/models.py
+++ kili/models.py
@@ -11,3 +11,18 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='product_category',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200,db_index=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=2,decimal_places=2)
+    posted = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200,blank=True)
+
+    class Meta:
+        ordering = ('category',)
+
+    def __str__(self):
+        return self.name
+
