5b67f02469b245612c3110fb4bd03e5de1eb8a92
irenecoder <wairene60@gmail.com>
2022-12-29 21:54:58 +0300

    products view

--- kili/models.py
+++ kili/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category,related_name='product_category',on_delete=models.CASCADE)
     name = models.CharField(max_length=200,db_index=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=2,decimal_places=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     posted = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200,blank=True)
--- kili/views.py
+++ kili/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Product,Category
 
 # Create your views here.
+def products(request,category_slug=None):
+    category = None 
+    categories = Category.objects.all()
+    products= Product.objects.filter('available')
+
+    if category_slug:
+        category = get_object_or_404(Category,slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request,'kili/home.html',context={'category':category,'categories':categories,'products':products})
+
