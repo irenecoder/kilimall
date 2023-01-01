3c15dfa22703f37b2d5d2b92a21c2b8a42f038e9
irenecoder <wairene60@gmail.com>
2023-01-01 22:25:58 +0300

    cart initial commits

--- kilimall/urls.py
+++ kilimall/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('kili.urls',namespace="kili")),
+    path('/', include('cart.urls',namespace='cart')),
 ]
