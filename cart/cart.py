0c52468a929ffe0e3fad7f90dc7bb97cb57c85b3
irenecoder <wairene60@gmail.com>
2023-01-06 21:31:43 +0300

    cart

--- cart/cart.py
+++ cart/cart.py
@@ -10,3 +10,9 @@ class Cart(object):
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
             self.cart = cart
+    def remove(self, product):
+        """Remove a product from the cart."""
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
