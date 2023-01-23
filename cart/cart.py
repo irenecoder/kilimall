56bf23be83965ab1fca3eba6b4cfe4219230e5d9
irenecoder <wairene60@gmail.com>
2023-01-23 14:49:36 +0300

    cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,18 +1,15 @@
 from decimal import Decimal
 from django.conf import settings
 from kili.models import Product
+
 class Cart(object):
-    def __init__(self, request):
-        """Initialize the cart."""
+    def __init__(self,request):
+        #initialize the session as a request
         self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
+        # try and get the cart in session
+        cart= self.session.get(settings.CART_SESSION_ID)
+
         if not cart:
-            # save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID] = {}
-            self.cart = cart
-    def remove(self, product):
-        """Remove a product from the cart."""
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
+            # save empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID]={}
+        self.cart = cart
