c398a49b431e575a18bb87daeba64fc80c1dac95
irenecoder <wairene60@gmail.com>
2023-01-25 18:38:09 +0300

    cart add view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.decorators.http import require_POST
+from .cart import Cart
+from .forms import CartAddProductForm
+from kili.models import Product
 
-# Create your views here.
+@require_POST
+def cart_add(request,product_id):
+    product = get_object_or_404(Product,id=product_id)
+    cart= Cart(request)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,quantity=cd['quantity'],override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
