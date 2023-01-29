a1e8dee8b33fce4c4e9a35777a53de8e2742f785
irenecoder <wairene60@gmail.com>
2023-01-29 11:27:24 +0300

    cart add

--- kili/views.py
+++ kili/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product,Category
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 def products(request,category_slug=None):
@@ -15,7 +16,8 @@ def products(request,category_slug=None):
 
 def product_detail(request,id,slug):
     product=get_object_or_404(Product,id=id,slug=slug,available=True)
-    return render(request,'kili/product.html',context={'product':product})
+    cart_product_form = CartAddProductForm()
+    return render(request,'kili/product.html',context={'product':product,'cart_product_form':cart_product_form})
 
 
 
