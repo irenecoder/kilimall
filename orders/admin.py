986c4296b4aaede0f5c1761fc9d35753b74a938b
irenecoder <wairene60@gmail.com>
2023-01-30 14:04:48 +0300

    order admin

--- orders/admin.py
+++ orders/admin.py
@@ -4,4 +4,11 @@ from .models import Orders
 # Register your models here.
 @admin.register(Orders)
 class OrdersAdmin(admin.ModelAdmin):
-    list_display = ['created_date', ]
+    list_display = ['id','first_name','last_name','city','email','address',
+                    'paid','updated_date','postal_code','created_date', ]
+    list_filter = ['paid','created_date','updated_date']
+    inlines = ['OrderItemInline']
+
+class OrderItemInline(admin.TabularInline):
+    model = Orders
+    raw_id_fields = ['product']
