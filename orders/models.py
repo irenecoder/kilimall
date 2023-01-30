1a39a66d837e4fc917ed9615d21980c137ef92b4
irenecoder <wairene60@gmail.com>
2023-01-30 13:46:07 +0300

    order item

--- orders/models.py
+++ orders/models.py
@@ -22,6 +22,19 @@ class Orders(models.Model):
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Orders,related_name='items',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name="items_ordered",on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def get_cost(self):
+        return self.quantity* self.price
+    
+
     
 
 
