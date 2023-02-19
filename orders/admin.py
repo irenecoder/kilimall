fe63b9f5f47afed0b75c5ac6b3c09a60dcae6a45
irenecoder <wairene60@gmail.com>
2023-02-19 22:28:06 +0300

    debug order admin

--- orders/admin.py
+++ orders/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from .models import Orders,OrderItem
 
 # Register your models here.
-@admin.register(Orders)
-class OrdersAdmin(admin.ModelAdmin):
-    list_display = ['id','first_name','last_name','city','email','address',
-                    'paid','updated_date','postal_code','created_date', ]
-    list_filter = ['paid','created_date','updated_date']
-    inlines = ['OrderItemInline']
-
-
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+@admin.register(Orders)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+    'address', 'postal_code', 'city', 'paid',
+    'created_date', 'updated_date']
+    list_filter = ['paid', 'created_date', 'updated_date']
+    inlines = [OrderItemInline]
+
